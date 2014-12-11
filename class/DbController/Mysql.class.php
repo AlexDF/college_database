0486930b8ef96bb7b64379b19f879878df589096
Alexandre DeFreitas <abd9@njit.edu>
2014-12-11 16:50:40 -0500

    created method to populate tables with csv data

--- class/DbController/Mysql.class.php
+++ class/DbController/Mysql.class.php
@@ -2,9 +2,12 @@
 
 namespace DbController;
 
+interface DbInterface {
+  function query($query);
+  function createTable($tbl_name, $headings);
+}
 
-
-class Mysql {
+class Mysql implements DbInterface {
 
   function query($query) {
     foreach($this->connection->query($query) as $row) {
@@ -17,16 +20,45 @@ class Mysql {
   function createTable($tbl_name, $headings) {
     $query = 'CREATE TABLE ' . '`' . $tbl_name . '`' . ' (';
     foreach($headings as $heading){
-      $query .= '`' . str_replace(' ', '_', $heading) . '`' . ' varchar(255)' . ', ';
+      $query .= '`' . str_replace(' ', '_', $heading) . '`' . ' varchar(255)';
+      if( $heading != end($headings) ){
+        $query .= ', ';
+      }
     }
-    $query .= 'FOREIGN KEY (`Unique_identification_number_of_the_institution`) REFERENCES schools(`Unique_identification_number_of_the_institution`)' . ')';
-    //$query .= ')';
+    
+    $query .= ')';
     
     $this->connection->query($query);
     print_r($this->connection->errorInfo());
-    echo $query;
+    
   }
 
+
+
+  function populateTable( $tbl_name, $records, $year=NULL ) {
+    foreach( $records as $record ) {
+      $query = 'INSERT INTO ' . $tbl_name . ' VALUES (';
+      foreach( $record as $datum ){
+        if($datum != 'R' && $datum != 'A' && $datum != 'Z') {
+            $query .= '"' . $datum . '"';
+            if( $datum != end($record) ) {
+              $query .= ', ';
+            }
+        }
+      }
+      
+      if($year){
+        $query .= '"' . $year . '"';
+      }
+      $query .= ')';
+      echo $query . '<br>';
+      $this->connection->query($query);
+      print_r($this->connection->errorInfo());
+      
+    }
+  
+
+  }
 
 
   function __construct() {
